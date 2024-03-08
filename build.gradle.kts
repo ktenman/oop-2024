9ae5d8223194669c0ebd9636eaab8f0c6b57ad02
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-03-08 12:21:02 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+group = "ee.ut"
+version = "0.0.1-SNAPSHOT"
+
+val assertjCoreVersion = "3.23.1"
+val junitJupiterVersion = "5.9.1"
+
+plugins {
+	java
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+	gradlePluginPortal() // Add the Gradle Plugin Portal repository
+}
+
+dependencies {
+	testImplementation("org.assertj:assertj-core:$assertjCoreVersion")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
